f9c3103902b966126838f97b9cc09eaf34a9b4f9
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-03-12 18:11:25 +0000

    Created

---
+++ MyMusic.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package algorithm.mymusic;
+
+/**
+ *
+ * @author niamh
+ */
+public class MyMusic {
+
+    public static void main(String[] args) {
+        MusicGUI music = new MusicGUI();
+        music.setVisible(true);
+    }
+}
