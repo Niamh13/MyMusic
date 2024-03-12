88b6ed7b24c50e950095f7a71ecd2ef06b4d28c5
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-03-12 18:35:58 +0000

    Update MyMusic.java

--- MyMusic.java
+++ MyMusic.java
@@ -1,17 +1,23 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
-package algorithm.mymusic;
+package mymusic;
 
 /**
- *
+ * MyMusic
  * @author niamh
+ * 12/03/2024
  */
 public class MyMusic {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        MusicGUI music = new MusicGUI();
-        music.setVisible(true);
+        MusicGUI mysong = new MusicGUI();
+        mysong.setVisible(true);
+        
     }
+    
 }
